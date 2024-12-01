35ef5d9bafb70c90a84bb37d63615a677d1c1bb9
Arunkumar Menon <arunkumar.menon@avisbudget.com>
2024-12-01 11:03:02 +0530

    adding project easyfix

---
+++ src/main/java/com/example/easyfix/EasyFixApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyFixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easyfix/EasyfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
