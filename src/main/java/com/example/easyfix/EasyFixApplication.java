06004a1d97dc3d7d04669df63afa0ae43b1afb84
Arunkumar Menon <arunkumar.menon@avisbudget.com>
2024-12-01 21:05:09 +0530

    adding .env file based on profile specific

--- src/main/java/com/example/easyfix/EasyFixApplication.java
+++ src/main/java/com/example/easyfix/EasyFixApplication.java
@@ -1,13 +1,19 @@
 package com.example.easyfix;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EasyFixApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EasyFixApplication.class, args);
-	}
+    public static void main(String[] args) {
+        String activeProfile = System.getenv("ACTIVE_PROFILE");
+        Dotenv dotenv = Dotenv.configure().filename(".env." + activeProfile).load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        SpringApplication.run(EasyFixApplication.class, args);
+    }
 
 }
